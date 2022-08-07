23e4946be99bba4f0507f8ff9cefc3742704ba48
Игорь Богдановский <igor.b@equilibrium.io>
2022-08-07 15:44:22 +0300

    add PartialEq, Eq traits for structs

--- src/common.rs
+++ src/common.rs
@@ -20,7 +20,7 @@ pub struct Winner {
 }
 
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
-#[derive(Clone, Copy, Decode, Encode, RuntimeDebug, TypeInfo)]
+#[derive(Clone, Copy, PartialEq, Eq, Decode, Encode, RuntimeDebug, TypeInfo)]
 pub struct Characteristics {
     pub health: u64,
     pub strength: u64,
@@ -33,21 +33,21 @@ pub struct Characteristics {
 }
 
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
-#[derive(Clone, Copy, Decode, Encode, RuntimeDebug, TypeInfo)]
+#[derive(Clone, Copy, PartialEq, Eq, Decode, Encode, RuntimeDebug, TypeInfo)]
 pub struct Skill {
     pub action_type: ActionType,
     pub level: u16,
 }
 
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
-#[derive(Clone, Decode, Encode, RuntimeDebug, TypeInfo)]
+#[derive(Clone, PartialEq, Eq, Decode, Encode, RuntimeDebug, TypeInfo)]
 pub struct Nft {
     pub characteristics: Characteristics,
     pub skills: Vec<Skill>,
 }
 
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
-#[derive(Clone, Decode, Encode, RuntimeDebug, TypeInfo)]
+#[derive(Clone, PartialEq, Eq, Decode, Encode, RuntimeDebug, TypeInfo)]
 pub struct Player {
     pub command: u8,
     pub player_id: u8,
@@ -64,7 +64,7 @@ pub struct InitGameState {
 }
 
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
-#[derive(Decode, Encode, RuntimeDebug, TypeInfo)]
+#[derive(Decode, Encode, PartialEq, Eq, RuntimeDebug, TypeInfo)]
 pub struct GameContext {
     pub auto_battler_core_version: AutoBattlerCoreVersion,
     pub max_turns: u64,
@@ -81,7 +81,7 @@ pub struct GameResult {
 }
 
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
-#[derive(Clone, Decode, Encode, RuntimeDebug, TypeInfo)]
+#[derive(Clone, PartialEq, Eq, Decode, Encode, RuntimeDebug, TypeInfo)]
 pub struct ActionState {
     pub players: [Vec<Player>; 2],
     pub action: ActionType,
@@ -89,7 +89,7 @@ pub struct ActionState {
 }
 
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
-#[derive(Clone, Decode, Encode, RuntimeDebug, TypeInfo)]
+#[derive(Clone, PartialEq, Eq, Decode, Encode, RuntimeDebug, TypeInfo)]
 pub struct TurnState {
     pub command_turn: u8,
     pub player_turn: [u8; 2],
